170fba4ac046f1cbe5ecd51cdfb722705c19e5a5
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-08-22 21:58:18 +0300

    'fix_header_scroll'

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
+const body = document.querySelector("body");
 const menuButton = document.querySelector("#menu-button");
 menuButton.addEventListener("click", event => {
     menuButton.classList.toggle("active")
+    body.classList.toggle("menu-active")
+    
 })
 document.querySelectorAll("a").forEach(tag => {
     tag.addEventListener("click", event => {
